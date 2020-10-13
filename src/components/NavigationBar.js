eddf23f280ef4f4b4e83365c611c5256bc07a50c
PeaWarrior <jacksonchen171@gmail.com>
2020-10-12 22:34:17 -0400

    Updated: index.html & NavigationBar.js, minor change to logo and name

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import pokeball from '../images/pokeball.png'
+import logo from '../images/logo.png'
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { NavLink} from 'react-router-dom';
 
@@ -7,11 +8,10 @@ export default function NavigationBar (props) {
     
     return (
         <Navbar bg="dark" variant="dark" className="d-flex justify-content-between">
-            <Navbar.Brand className="ml-5"> <img src={pokeball} alt="" style={{width: 'auto'}}/> Pokebattle</Navbar.Brand>
+            <Navbar.Brand className="ml-5"> <img src={pokeball} alt="" style={{width: 'auto'}}/> <img src={logo} alt="" style={{width: '150px'}}/></Navbar.Brand>
             {props.currentUser ? 
                 <>
                 <Nav className="mr-auto">
-                    {/* <NavLink className="ml-5" to="/">Home</NavLink> */}
                     <NavLink className="ml-5" to="/teams">My Teams</NavLink>
                     <NavLink className="ml-5" to="/playground">Playground</NavLink>
                 </Nav>
